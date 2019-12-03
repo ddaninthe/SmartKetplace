b476289db5ce82a1eabd066a87793b6e0b4b454e
ddaninthe <d.daninthelopez@gmail.com>
2019-12-03 20:23:09 +0100

    Client setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SellComponent } from './sell/sell.component';
+
+//Définition des routes
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sell', component: SellComponent },
+  { path: '**', redirectTo: '' } //Redirection à la page d'accueil pour routes invalides
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SellComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
